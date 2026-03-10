af9e038f093149964f79fe2b2de82c21d8cbd98e
Tarangladumor <tarangladumor@gmail.com>
2026-03-11 01:01:05 +0530

    Added frontend,backend,websockets

---
+++ apps/backend/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import { prismaClient } from "db/client";
+
+const app = express();
+
+
+app.use(express.json())
+
+app.get("/users", (req, res) => {
+    prismaClient.users.findMany()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
+})
+
+app.post("/user", (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.status(400).json({ message: "Username and passwrod are required" })
+        return
+    }
+
+    prismaClient.user.create({
+        data: {
+            username,
+            password
+        }
+    })
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message })
+        })
+
+})
+
+app.listen(8080)
